158dc58ca18ed878ff213caa78425017ea59db34
Aditya Thakur <71738434+athakr@users.noreply.github.com>
2022-11-17 09:34:00 -0500

    updated engine
    a file output to update betas, and a stock picker based on risk level

--- src/main/python/engine.py
+++ src/main/python/engine.py
@@ -1,7 +1,22 @@
 import numpy as np
 import yfinance as yf
 import pandas as pd
+from logging import ERROR
+from json.decoder import JSONDecodeError
 
+class Track:
+  def __init__(self, stock):
+    self.stock = stock
+  def info(self):
+    self.data = yf.Ticker(self.stock)
+    self.infor = self.data.info
+    self.beta = 0
+
+    for key in self.infor:
+      if(key == 'beta'):
+        self.beta = self.infor[key]
+
+    return self.beta
 
 class Engine():
     def __init__(self, risk, dollars):
@@ -20,6 +35,7 @@ class Engine():
         self.budget = dollars
         #builds a base portfolio with associated risks amount for various risk types. index of [risk-1] is associated with the specific
         #risk listed above
+        
         port_risk = {
             "US Stock":[0.14, 0.35, 0.49, 0.6],
             "Foreign Stock":[0.06, 0.15, 0.21, 0.25],
@@ -27,10 +43,10 @@ class Engine():
             "Short-term investments":[0.3, 0.1, 0.05, 0]
         }
 
-        alloc = pd.DataFrame(port_risk) #make a Dataframe
+        self.alloc = pd.DataFrame(port_risk) #make a Dataframe
 
         #Take a specific row associated with given risk level
-        personal_alloc = alloc.loc[risk-1]
+        personal_alloc = self.alloc.loc[risk-1]
 
         #make a portfolio based on risk and amount up for investment
         d_alloc = {
@@ -50,6 +66,66 @@ class Engine():
         else:
             print(self.dol_alloc)
 
+    def dollar_update(self, risk, dollars):
+        self.risk = risk
+        self.budget = dollars
+        personal_alloc = self.alloc.loc[risk-1]
 
+        #make a portfolio based on risk and amount up for investment
+        d_alloc = {
+            "US Stock": [(personal_alloc.iloc[0] * self.budget)],
+            "Foreign Stock": [(personal_alloc.iloc[1] * self.budget)],
+            "Bond": [(personal_alloc.iloc[2] * self.budget)],
+            "Short-term investments": [(personal_alloc.iloc[3] * self.budget)]
+        }
+        
+        #make it into a Dataframe
+        self.dol_alloc = pd.DataFrame(d_alloc)
+        self.residualDisplay()
+
+    def update_beta(self):
+        path1 = 'DAQ.csv'
+        path2 = 'SP500.csv'
+        path3 = 'DOW.csv'
+
+        daq = pd.read_csv(path1)
+        DAQ = pd.DataFrame(daq)
+        DAQ = DAQ.drop(columns=["Description", 'Category2', 'Category3', 'GICS Sector', 'Country', 'Market cap', 'Action'])
+
+
+        sp = pd.read_csv(path2)
+        SP500 = pd.DataFrame(sp)
+        SP500 = SP500.drop(columns=["Description", 'Category2', 'Category3', 'GICS Sector','Price to TTM earnings',
+            'Price to TTM sales', 'Price to book value', 'Market cap', 'Action'])
+
+        dow = pd.read_csv(path3)
+        DOW = pd.DataFrame(dow)
+        DOW = DOW.drop(columns=["Description", 'Category2', 'GICS Sector', 'Country', 'Market cap','Market Cap Weight', 'Index Weight', 'Action'])
+
+        stock_list = pd.merge(DOW, DAQ, how='outer', left_on=["Symbol", 'Dividend yield'], right_on=["Symbol", 'Dividend yield'])
+        stock_list = pd.merge(stock_list, SP500, how='outer', left_on=["Symbol", 'Dividend yield'], right_on=["Symbol", 'Dividend yield'])
+
+        beta_list = np.array([])
+
+        for i in range(0, len(stock_list)):
+            try:
+                k = stock_list['Symbol'][i]
+                z = Track(k)
+                m = z.info()
+                beta_list = np.append(beta_list, m)
+            except TypeError:
+                beta_list = np.append(beta_list, -100)
+
+        stock_list['Beta'] = beta_list
+        stock_list = stock_list.loc[(stock_list['Beta'] >= 0)]
+
+        stock_list = stock_list.sort_values(by='Beta', ascending=True)
+        stock_list.to_csv('stock_list.csv')
+    def get_engine_portfolio(self):
+        path = 'stock_list.csv'
+        stocks = pd.DataFrame(pd.read_csv(path))
+        self.cons = stocks.loc[(stocks['Beta'])]
+#Testing#
 Test1 = Engine(1, 1000)
 Test1.residualDisplay()
+Test1.dollar_update(2, 2000)
